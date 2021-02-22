9e891a92a0cb13e073156d3a46ab7578d32ef1f1
“SagarSharma0297” <sharma.sagar0297@gmail.com>
2021-02-22 17:27:50 +0530

    Updates

---
+++ precache-manifest.1618117c4d92a9c11f6510b754356503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4db2b5dbfe7ce8562389",
+    "url": "/react-kafene-app/static/css/main.c176642f.chunk.css"
+  },
+  {
+    "revision": "4db2b5dbfe7ce8562389",
+    "url": "/react-kafene-app/static/js/main.679de7cd.chunk.js"
+  },
+  {
+    "revision": "dd9c4d08563e4a6e3d3f",
+    "url": "/react-kafene-app/static/js/runtime~main.d9f504d9.js"
+  },
+  {
+    "revision": "b1c32e1eb075e64476d4",
+    "url": "/react-kafene-app/static/js/2.5d38955d.chunk.js"
+  },
+  {
+    "revision": "3103e59a16f52c210ebb",
+    "url": "/react-kafene-app/static/js/3.2518016f.chunk.js"
+  },
+  {
+    "revision": "8a08b7aaa5fa5ae3bc1300dae83767fe",
+    "url": "/react-kafene-app/index.html"
+  }
+];
